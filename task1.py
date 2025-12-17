07565a877d4134a4a3e7ff9dd43ff5d2112ad96f
terrycolgate337-oss <terry.colgate337@students.cmcss.net>
2025-12-17 09:17:22 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,33 @@
+#Given variables
+party_pizza_mini = 14
+large = 8
+medium = 6
+people = 6 # friends
 
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+mini_party_pizza = party_pizza_mini
+large_pizza = large
+medium_pizza = medium
+
+
+# TODO Update people to include yourself
+# TODO Calculate share
+# TODO Calculate leftover
+# TODO Print share result
+# TODO print leftover result
+
+# TODO Update people #Eric and Brandon are coming too.
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share results
+# TODO Print leftover result
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+# TODO Update slices
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share result
+# TODO Print leftover result
+print("...for Mr. Hollow Leg")
